1268befac2f521847a66f4f69d98d5c18ccb2af4
xiekuozhi <xiekuozhi@163.com>
2022-06-29 18:23:02 +0800

    deploy

---
+++ precache-manifest.1fe489843dc2a39d3aa17dd86db6bd88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8849e053ceb4afd6fb97",
+    "url": "/moeney-3-website/css/app.1effad28.css"
+  },
+  {
+    "revision": "e4394e5dad8ce3a9a8947b37ee5553e2",
+    "url": "/moeney-3-website/index.html"
+  },
+  {
+    "revision": "8849e053ceb4afd6fb97",
+    "url": "/moeney-3-website/js/app.b69bf781.js"
+  },
+  {
+    "revision": "f673c0b15e8cf9a0bdbf",
+    "url": "/moeney-3-website/js/chunk-vendors.717e24ca.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/moeney-3-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moeney-3-website/robots.txt"
+  }
+]);
